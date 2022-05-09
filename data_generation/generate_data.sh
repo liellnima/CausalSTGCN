a129c740dc18ce070b6f036e9bb2d3f30bb69b75
liellnima <jkaltenborn@uos.de>
2022-05-09 11:43:35 -0400

    added dir in front of command because structure changed

--- data_generation/generate_data.sh
+++ data_generation/generate_data.sh
@@ -8,13 +8,13 @@ i=0
 for timewindow in 100 500; do
   for p in 0.3 0.6 0.9; do
     echo $i
-    python graph_generator.py --exp-id $i --n 1000 --num-features 5 -g 20 -p $p --neighborhood 2 --timewindow $timewindow --eta 1.5
+    python data_generation/graph_generator.py --exp-id $i --n 1000 --num-features 5 -g 20 -p $p --neighborhood 2 --timewindow $timewindow --eta 1.5
     let i++
-    python graph_generator.py --exp-id $i --n 1000 --num-features 5 -g 20 -p $p --neighborhood 5 --timewindow $timewindow --eta 1.5
+    python data_generation/graph_generator.py --exp-id $i --n 1000 --num-features 5 -g 20 -p $p --neighborhood 5 --timewindow $timewindow --eta 1.5
     let i++
-    python graph_generator.py --exp-id $i --n 1000 --num-features 5 -g 100 -p $p --neighborhood 10 --timewindow $timewindow --eta 1.5
+    python data_generation/graph_generator.py --exp-id $i --n 1000 --num-features 5 -g 100 -p $p --neighborhood 10 --timewindow $timewindow --eta 1.5
     let i++
-    python graph_generator.py --exp-id $i --n 1000 --num-features 5 -g 100 -p $p --neighborhood 25 --timewindow $timewindow --eta 1.5
+    python data_generation/graph_generator.py --exp-id $i --n 1000 --num-features 5 -g 100 -p $p --neighborhood 25 --timewindow $timewindow --eta 1.5
     let i++
   done
 done
